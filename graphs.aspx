603122eb1a945a88d543cc239407b2b7d86bd3d9
Tanishq <onepowerinfinity@gmail.com>
2017-10-27 02:35:41 +0530

    Added GRAPHS

--- graphs.aspx
+++ graphs.aspx
@@ -3,7 +3,7 @@
 <%@ Register Assembly="System.Web.DataVisualization, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" Namespace="System.Web.UI.DataVisualization.Charting" TagPrefix="asp" %>
 
 <asp:Content ContentPlaceHolderID="HomePage" runat="server">
-    <asp:Chart runat="server" ID="char1">
+    <%--<asp:Chart runat="server" ID="char1">
         <Series>
             <asp:Series Name="Series1"></asp:Series>
         </Series>
@@ -13,12 +13,12 @@
     </asp:Chart>
     <br />
     <br />
-    <br />
+    <br />--%>
     <asp:DropDownList Width="100px" ID="medchart" runat="server" OnSelectedIndexChanged="medchart_SelectedIndexChanged" AutoPostBack="true"></asp:DropDownList>
     <br />
     <br />
     <br />
-    <asp:Chart ID="Chart2" runat="server" >
+    <asp:Chart ID="Chart2" runat="server" Width="500px" >
         <Series>
             <asp:Series Name="Series1"></asp:Series>
         </Series>
@@ -26,5 +26,7 @@
             <asp:ChartArea Name="ChartArea1"></asp:ChartArea>
         </ChartAreas>
     </asp:Chart>
-
+    <br />
+    <br />
+    <asp:Label ID="er" runat="server"></asp:Label>
 </asp:Content>
--- graphs.aspx.cs
+++ graphs.aspx.cs
@@ -36,13 +36,42 @@ public partial class graphs : System.Web.UI.Page
         //con.Open();
         //SqlCommand command = new SqlCommand("Select Sales.Purchase_Date, SUM() from MedicineMaster", con);
         //SqlDataReader reader = command.ExecuteReader();
-        if(medchart.SelectedValue != "Month")
+        if (medchart.SelectedValue != "Month" && medchart.SelectedValue != "Select")
         {
             SqlConnection con = new SqlConnection(WebConfigurationManager.ConnectionStrings["medDb"].ConnectionString);
-        }
-        else
-        {
+            con.Open();
+            SqlCommand command = new SqlCommand("Select Sales.Purchase_Date,SUM(SalesInfo.Quantity) as whatis from Sales,SalesInfo where SalesInfo.Sales_ID = Sales.Sales_ID AND SalesInfo.Med_ID=@id and  Sales.Purchase_Date > @temp group by  Sales.Purchase_Date ", con);
+            command.Parameters.AddWithValue("@temp", DateTime.Now.AddDays(-30));
+            command.Parameters.AddWithValue("@id", medchart.SelectedValue);
+            SqlDataReader reader = command.ExecuteReader();
+            if(!reader.Read())
+            {
+                er.Text = "No Value Found For the MedID";
+            }
+            reader.Close();
+            SqlDataReader reader2 = command.ExecuteReader();
+            Chart2.DataSource = reader2;
+            Chart2.Series["Series1"].XValueMember = "Purchase_Date";
+            Chart2.Series["Series1"].YValueMembers = "whatis";
+            Chart2.ChartAreas[0].AxisX.LabelStyle.Format = "MM-dd";
+            Chart2.DataBind();
+
+            reader2.Close();
 
         }
+        else if(medchart.SelectedValue != "Select")
+        {
+            SqlConnection con = new SqlConnection(WebConfigurationManager.ConnectionStrings["medDb"].ConnectionString);
+            con.Open();
+            SqlCommand command = new SqlCommand("Select Sales.Purchase_Date,SUM(SalesInfo.Quantity) as whatis from Sales,SalesInfo where SalesInfo.Sales_ID = Sales.Sales_ID and  Sales.Purchase_Date > @temp group by  Sales.Purchase_Date ", con);
+            command.Parameters.AddWithValue("@temp", DateTime.Now.AddDays(-30));
+            SqlDataReader reader = command.ExecuteReader();
+            Chart2.DataSource = reader;
+            Chart2.Series["Series1"].XValueMember = "Purchase_Date";
+            Chart2.Series["Series1"].YValueMembers = "whatis";
+            Chart2.ChartAreas[0].AxisX.LabelStyle.Format = "MM-dd";
+            Chart2.DataBind();
+            reader.Close();
+        }   
     }
 }
